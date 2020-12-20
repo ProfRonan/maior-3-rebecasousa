84c559454b729ecbd31f6467255d71f82d941b61
rebecasousa1 <rebecasousa@alu.ufc.br>
2020-12-20 18:20:28 +0000

    concluído.

--- main.py
+++ main.py
@@ -4,7 +4,18 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
+    x = float(input("Digite o primeiro número:"))
+    y = float(input("Digite o segundo número:"))
+    z = float(input("Digite o terceiro número:"))
 
+    if x >= y and x >= z:
+      maior = x
+    elif y >= x and y >= z:
+      maior = y
+    else:
+      maior = z
+
+    print(f"{maior}")      
 
 if __name__ == '__main__':
     main()
